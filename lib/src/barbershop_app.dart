96de244d5cafcb74f45b6e5a60f16dc8619c86e9
CarlosMeyer2021 <carlos.desenvolve@gmail.com>
2023-09-01 09:43:20 -0300

    DW_barbershop

---
+++ lib/src/barbershop_app.dart
@@ -0,0 +1,29 @@
+import 'package:asyncstate/observers/async_navigator_observer.dart';
+import 'package:asyncstate/widget/async_state_builder.dart';
+import 'package:dw_barbershop/src/core/ui/widgets/barbershop_loader.dart';
+import 'package:dw_barbershop/src/features/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+import 'features/auth/login/login_page.dart';
+
+class BarbershopApp extends StatelessWidget {
+  const BarbershopApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AsyncStateBuilder(
+      customLoader: const BarberShopLoader(),
+      builder: (asyncNavigatorObserver) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'DW BarberShop',
+          navigatorObservers: [asyncNavigatorObserver],
+          routes: {
+            '/': (_) => const SplashPage(),
+            '/auth/login': (_) => const LoginPage(),
+          },
+        );
+      },
+    );
+  }
+}
